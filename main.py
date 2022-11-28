8a186aa776189912c417326f1c48b27eed810b7d
Christian Simăn-Cherecheș <49637374+callmechristian@users.noreply.github.com>
2022-11-28 16:22:40 +0100

    basic classifier

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import numpy as np
 import os
 
 df = pd.read_csv(r'data/data_processed.csv')
+df_continous = pd.read_csv(r'data/data_continous.csv')
+df_categorical = pd.read_csv(r'data/data_categorical.csv')
 
-allColumnsNames = np.array(df.columns)
-
+# --------------------------------------------------------------------------------------------------
 # copying job satisfaction into new array
-JSat = df[allColumnsNames[17]].values
-# drop job satisfacftion column from input data
-df.drop(allColumnsNames[17], axis=1)
+JSat = df['JobSatisfaction'].values
 
 # splitting inputs by row index
-df_training = df.iloc[:1400,:]
-df_validation = df.iloc[1400:,:]
+# continous data
+df_training_continous = df_continous.iloc[:1400,:]
+df_validation_continous = df_continous.iloc[1400:,:]
+# categorical data
+df_training_categorical = df_categorical.iloc[:1400,:]
+df_validation_categorical = df_categorical.iloc[1400:,:]
 # splitting outputs by number
 JSat_training = JSat[:1400]
 JSat_validation = JSat[1400:]
-print("Shape of new inputs - {} , {}".format(df_training.shape, df_validation.shape))
-print("Shape of new outputs - {} , {}".format(JSat_training.shape, JSat_validation.shape))
-
+# --------------------------------------------------------------------------------------------------
 # tunableColumnsNames = np.array([allColumnsNames[2],allColumnsNames[3],allColumnsNames[12],allColumnsNames[14],allColumnsNames[18],allColumnsNames[19],allColumnsNames[26],allColumnsNames[27],allColumnsNames[29],allColumnsNames[33],allColumnsNames[34]])
 # print(tunableColumnsNames)
 
@@ -35,12 +36,21 @@ print("Shape of new outputs - {} , {}".format(JSat_training.shape, JSat_validati
 
 # Random Forests
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
 
-# df_training, JSat_training = make_classification(n_estimators=500, bootstrap=False)
-clf = RandomForestClassifier(max_depth=None, random_state=None)
-clf.fit(df_training,JSat_training)
-JSat_predicted = clf.predict(df_validation)
-print(clf.score(df_validation, JSat_validation)) 
+# continous data classifier
+clf_continous = RandomForestClassifier(max_depth=None, random_state=None)
+
+clf_continous.fit(df_training_continous,JSat_training)
+acc_continous = clf_continous.score(df_validation_continous, JSat_validation)
+
+# categorical data classifier
+clf_categorical = RandomForestClassifier(max_depth=None, random_state=None)
+
+clf_categorical.fit(df_training_categorical,JSat_training)
+acc_categorical = clf_categorical.score(df_validation_categorical, JSat_validation)
+
+# print results
+print('Continous data accuracy: ', acc_continous)
+print('Categorical data accuracy: ', acc_categorical)
 
 # print(allColumnsNames)
